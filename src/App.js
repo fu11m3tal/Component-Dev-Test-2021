94ecfb40255a0267661a5844e8794a81fdee6162
Steven Yoh <yoh@stevens-mbp.lan>
2021-03-22 15:35:51 -0700

    Content loaded from the provided JSON file and bound to the template via the component's state/props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import content from './content.json';
+import backgrounds from './backgrounds/slide_one.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      content: content,
+      main: 0
+     };
+     this.setMain = this.setMain.bind(this);
+  }
+  
+  setMain(e) {
+    console.log(backgrounds)
+    var newMain = e.target.id;
+    this.setState({main:newMain})
+  }
+  render() {
+    var { content, main } = this.state;
+    var { pages } = content;
+    var page = pages[main]
+    var {title, slug, blocks} = page;
+    var block = blocks[0];
+    var {type, headline, subhead, cta, background} = block;
+    console.log(background)
+    return (
+      <div className="App">
+        <nav>
+          {content.pages.map((item, index) => (
+            <div>
+              <button id={index} className={item.title} onClick={this.setMain}>
+                {item.title}
+              </button>
+            </div>
+          ))}
+        </nav>
+        <header className="App-header">
+          {page.title}
+          <br></br>
+          {page.blocks[0].subhead}
+        </header>
+        <div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
